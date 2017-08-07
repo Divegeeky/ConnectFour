ac2982e45dda27f441c1ecf348e6d05b482a83d3
jonathon-ross <jonathon.m.ross1@gmail.com>
2017-08-07 01:27:40 -0700

    Update ReadableBoard.java

--- ConnectFourGame/src/connect/four/board/ReadableBoard.java
+++ ConnectFourGame/src/connect/four/board/ReadableBoard.java
@@ -2,7 +2,14 @@
 package connect.four.board;
 
 import connect.four.player.Player;
-
+/**
+ * SER 216 - Group Project
+ * 
+ * <p>This class sets the minimum methods that all classes implementing it must contain.
+ * 
+ * @author SER-216 Instructors
+ * @version 8/7/2017
+ */
 public interface ReadableBoard {
     Player whoPlayed(int x, int y);
     int getWidth();
