05dd19c09194aa40b65e25f168a13d6beea74974
jonathon-ross <jonathon.m.ross1@gmail.com>
2017-08-07 01:25:36 -0700

    Update Board.java

--- ConnectFourGame/src/connect/four/board/Board.java
+++ ConnectFourGame/src/connect/four/board/Board.java
@@ -1,13 +1,28 @@
 package connect.four.board;
-
 import connect.four.player.Player;
 import java.util.Arrays;
 
+/**
+ * SER 216 - Group Project
+ * 
+ * <p>This class produces an mxn game board from user input. It has functionality to place
+ * a game piece, clear the board, return player objects.
+ * 
+ * @author Jonathon Ross, SER-216 Instructors
+ * @version 8/7/2017
+ */
 public class Board implements ReadWritableBoard {
     private Player[][] m_contents;
     private int m_moveCount;
     
-	public Board(int width, int height) {
+    /**
+     * Constructor for a Board object. To be used when no previous board exists. Creates a mxn Player array. Will throw a NegativeArraySizeException if
+     * either width or height are a negative number.
+     * 
+     * @param width
+     * @param height
+     */
+	public Board(int width, int height) throws NegativeArraySizeException {
     	if(width < 0)
     	{
     		throw new NegativeArraySizeException("The width was negative. Please enter a number >= 0");
@@ -21,7 +36,17 @@ public class Board implements ReadWritableBoard {
     	}
         m_moveCount = 0;
     }
+	
+	/**
+	 * Constructor for a Board object. This is to be used if there is already an existing Board that needs to be 
+	 * used to set up a new Board.
+	 *  
+	 * @param copy
+	 */
     public Board(ReadableBoard copy) {
+    	
+    	//Copies a Board Object
+    	
         if (copy instanceof Board) {
         	Board copyB = (Board) copy;
             m_moveCount = copyB.getMoveCount();
@@ -31,7 +56,11 @@ public class Board implements ReadWritableBoard {
             for (int i = 0; i != l; ++i) {
                 m_contents[i] = Arrays.copyOf(copyB.mContentsArray(i), m);
             }
-        } else {
+        } 
+        
+        // Copies any other ReadableBoard Object
+        
+        else {
         	int l = copy.getWidth();
             int m = copy.getHeight();
             m_contents = new Player[l][m];
@@ -43,6 +72,13 @@ public class Board implements ReadWritableBoard {
             }
         }
     }
+    
+    /**
+     * Method returns the Player object stored within the mxn array that is located at [x][y].
+     * 
+     * @param x
+     * @param y
+     */
     public @Override Player whoPlayed(int x, int y) {
     	if(x < 0 || x > (getWidth()-1) || y < 0 || y > (getHeight()-1)){
     		return null;
@@ -50,14 +86,31 @@ public class Board implements ReadWritableBoard {
     	else
     		return m_contents[x][y];
     }
+    /**
+     * Method returns the width of the Board object.
+     */
     public @Override int getWidth() {
         return m_contents.length;
     }
+    /**
+     * Method returns the height of the Board object.
+     */
     public @Override int getHeight() {
         return m_contents[0].length;
     }
-    public @Override void play(int x, Player p) {
+    
+    /**
+     * Method simulates placing a game piece in the board. Stores Player p into the appropriate index within
+     * the column designated by x.
+     * 
+     * @param x
+     * @param p
+     */
+    public @Override void play(int x, Player p) throws ColumnFullException{
         int y = getColumnHeight(x);
+        
+        //Checks to see if the column is full
+        
         if (y == m_contents[x].length) {
             throw new ColumnFullException();
         }
@@ -65,6 +118,11 @@ public class Board implements ReadWritableBoard {
         m_moveCount += 1;
     }
     
+    /**
+     * Method gets the number of game pieces within the column designated by x.
+     * 
+     * @param x
+     */
     public @Override int getColumnHeight(int x){
         int y = 0;
 	int l = m_contents[0].length;
@@ -75,6 +133,10 @@ public class Board implements ReadWritableBoard {
         }
         return y;
     }
+    
+    /**
+     * Clears the current board by setting each column to a new Player array.
+     */
     public @Override void clear() {
         int l = m_contents.length;
         int m = m_contents[0].length;
@@ -83,12 +145,20 @@ public class Board implements ReadWritableBoard {
         }
 	m_moveCount = 0;
     }
+    
+    /**
+     * Method returns the moveCount
+     */
     public @Override int getMoveCount() {
         return m_moveCount;
     }
-    public Player mContentsPlayer(int x, int y) {
-    	return m_contents[x][y];
-    }
+    
+    /**
+     * Method returns the entire column designated by x.
+     * 
+     * @param x
+     * @return
+     */
     public Player[] mContentsArray(int x) {
     	return m_contents[x];
     }
