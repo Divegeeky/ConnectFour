28993ead0cb41f69e223cab7c5b3f62c590cdd2d
jonathon-ross <jonathon.m.ross1@gmail.com>
2017-08-07 01:28:33 -0700

    Update ReadWritableBoard.java

--- ConnectFourGame/src/connect/four/board/ReadWritableBoard.java
+++ ConnectFourGame/src/connect/four/board/ReadWritableBoard.java
@@ -1,7 +1,14 @@
 
 package connect.four.board;
 
-
+/**
+ * SER 216 - Group Project
+ * 
+ * <p>This class creates an interface that combines the ReadableBoard and WritableBoard Interfaces.
+ * 
+ * @author SER-216 Instructors
+ * @version 8/7/2017
+ */
 public interface ReadWritableBoard extends ReadableBoard, WritableBoard {
 	
 }
