3ebe34447ace33653bc17e7114f5a0bf64e05ca7
Ovadia Shalom <ovidubs@gmail.com>
2017-08-08 21:39:12 -0500

    Update ConsolePlayer.java

--- ConnectFourGame/src/connect/four/player/ConsolePlayer.java
+++ ConnectFourGame/src/connect/four/player/ConsolePlayer.java
@@ -2,7 +2,6 @@
 package connect.four.player;
 import connect.four.board.ReadableBoard;
 import connect.four.board.ReadWritableBoard;
-import connect.four.Game;
 import connect.four.ScoreChart;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -32,7 +31,8 @@ public class ConsolePlayer implements Player, ScoreChart.Listener {
 
     @Override public void performPlay(ReadWritableBoard board) {
         int width = board.getWidth();
-        int height = board.getHeight();
+        //height var not needed.
+        //int height = board.getHeight();
 
         System.out.println("\n"+m_name+"'s turn!");
         dumpBoard(board);
@@ -44,9 +44,9 @@ public class ConsolePlayer implements Player, ScoreChart.Listener {
                 System.out.print("Enter the column you want to play in: ");
                 x = Integer.parseInt(stdin.readLine());
             } catch (IOException e) {
-                // loop again.
+                System.out.println(e.getMessage() + " --> Invalid Input/Output");
             } catch (NumberFormatException e) {
-                // loop again.
+                System.out.println(e.getMessage() + " --> Incorrect format input");
             }
         }
 
@@ -57,13 +57,13 @@ public class ConsolePlayer implements Player, ScoreChart.Listener {
         int width = board.getWidth();
         int height = board.getHeight();
 
-        System.out.println("@ is you, X is the other player, and O is empty.");
+        //System.out.println("@ is you, X is the other player, and O is empty.");
         for (int i = height-1; i != -1; --i) {
             for (int j = 0; j != width; ++j) {
                 Player played = board.whoPlayed(j, i);
                 System.out.print(
-                    played == this ? "@" :
-                    played == null ? "O" : "X"
+                    played == this ? "Player 1" :
+                    played == null ? "Player 2" : "Empty"
                 );
             }
             System.out.println();
